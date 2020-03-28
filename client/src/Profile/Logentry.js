6cc4b8d83a2f29fceafc215e1616df1858cbb0a5
YoonSoon Kang <kangyoonsoon@hotmail.com>
2020-03-28 16:55:39 -0400

    initial container of Profile container

--- client/src/Profile/Logentry.js
+++ client/src/Profile/Logentry.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./Profile.scss";
+import "./Logentry.scss";
 
-export default function Logentry( props ) {
+export default function Logentry(props) {
   const {
     created_at,
     video_id,
@@ -27,24 +27,26 @@ export default function Logentry( props ) {
   // console.log("Logitem", log);
   return (
     <div className="Logentry_container">
-      <h2 className="Logentry_emotionBeforeVideo">Your Emotion Before the Video was: {props.emotionBeforeVideo}</h2>
-      <img className="Logentry_img" src={props.img}  width="150" height="100">   
-      </img>
-      <ul className="Logentry_reaction">
-        <li> 😐 {props.neutral_percent}%</li>
-        <li> 😡{props.angry_percent}%</li>
-        <li> 😁{props.happy_percent}%</li>
-        <li> 😢{props.sad_percent}%</li>
-        <li> 😱{props.fearful_percent}%</li>
-        <li> 🤢{props.disgusted_percent}%</li>
-        <li>😲{props.surprised_percent}%</li>
-      </ul>
+      <h2 className="Logentry_emotionBeforeVideo">
+        Your Emotion Before the Video was: {props.emotionBeforeVideo}
+      </h2>
+      <div className="Logentry_content">
+        <img
+          className="Logentry_img"
+          src={props.img}
+          width="150"
+          height="100"
+        ></img>
+        <ul className="Logentry_reaction">
+          <li> 😐{props.neutral_percent}%</li>
+          <li> 😡{props.angry_percent}%</li>
+          <li> 😁{props.happy_percent}%</li>
+          <li> 😢{props.sad_percent}%</li>
+          <li> 😱{props.fearful_percent}%</li>
+          <li> 🤢{props.disgusted_percent}%</li>
+          <li> 😲{props.surprised_percent}%</li>
+        </ul>
+      </div>
     </div>
-
-
-
-     
   );
 }
-
-
